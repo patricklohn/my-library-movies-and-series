3dca5f42299041d78016f09ca8851aa3358c891d
Patrick Lohn <patricklohn3@gmail.com>
2024-11-10 12:31:30 -0300

    Finalização de ajustes principais

--- serve.js
+++ serve.js
@@ -1,2 +1,14 @@
 const express = require('express');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 5000;
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    console.log("passou")
+})
+
+app.listen(port,() =>{
+    console.log(`Programa rodando na porta ${port}. Acesse o link do serviço http://localhost:${port}/`)
+})
