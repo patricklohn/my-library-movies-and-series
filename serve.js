ec2551c8a8fa11592254490d29ee8f5a122da0ae
Patrick Lohn <patricklohn3@gmail.com>
2024-11-10 16:18:38 -0300

    criação das rotas e criação das paginas

--- serve.js
+++ serve.js
@@ -5,8 +5,31 @@ const app = express();
 const port = 5000;
 app.use(express.static('public'))
 
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+app.engine('handlebars', hbs.engine)
+app.set ('view engine', 'handlebars')
+
+const meioBusca = 'https://www.google.com/search?q=';
+
+app.get('/filmes',(req,res)=>{
+    const modals = [
+        {
+            name: 'Carandiru: O Filme',
+            linkImg: 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/00/05/19870969.jpg',
+            category: 'Crime/Drama',
+            description: 'Médico sanitarista se oferece para realizar o trabalho de prevenção ao vírus HIV no Carandiru, maior presídio da América Latina, durante a década de 1990. Convivendo diariamente com a dura realidade dos detentos, ele presencia a violência agravada pela superlotação, a precariedade dos serviços prestados e a animalização dos presos. Paradoxalmente, ele conhece o sistema de organização interna e o lado frágil, romântico e sonhador dos homens cumprindo pena.',
+            nota: '6',
+            searchGoogle: `${meioBusca}Carandiru:+O+Filme`
+        }
+    ]
+
+    res.render("filmes",{modals})
+})
+
 app.get('/',(req,res)=>{
-    console.log("passou")
+    res.render('home')
 })
 
 app.listen(port,() =>{
